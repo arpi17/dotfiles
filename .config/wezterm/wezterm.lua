1ab1c97e1bb09805f2d5c2edf0c9138d3c257402
Arpad Illyes <arpi@fastmail.com>
2025-05-16 11:54:54 +0200

    UI tweaks and numbered tab selector keymap

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,18 +7,21 @@ local config = wezterm.config_builder()
 config.color_scheme = "tokyonight_moon"
 
 -- Background opacity
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.80
 
 -- Font
 config.font = wezterm.font("Monaspace Argon")
 
 -- Tab bar
 config.hide_tab_bar_if_only_one_tab = false
-config.tab_bar_at_bottom = true
+config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
 
 -- Remove top window bar
 config.window_decorations = "RESIZE"
+
+-- Resize behaviour
+config.adjust_window_size_when_changing_font_size = false
 
 -- Scrollback lines
 config.scrollback_lines = 1000
@@ -113,6 +116,15 @@ config.keys = {
 		}),
 	},
 }
+
+-- Move to numbered tab
+for i = 1, 8 do
+	table.insert(config.keys, {
+		mods = "LEADER",
+		key = tostring(i),
+		action = act.ActivateTab(i - 1),
+	})
+end
 
 config.key_tables = {
 	resize_pane = {
