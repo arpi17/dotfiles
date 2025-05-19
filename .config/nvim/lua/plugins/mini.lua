21dfaed7b111df8dfa1fff8ff681ff9451a5d6b6
Arpad Illyes <arpi@fastmail.com>
2025-05-19 16:13:42 +0200

    Add mini.diff to show version control changes

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,19 +1,29 @@
 return {
-    'echasnovski/mini.nvim',
-    config = function()
-	require('mini.statusline').setup() 
-	-- Better Around/Inside textobjects
-	--
-	-- Examples:
-	--  - va)  - [V]isually select [A]round [)]paren
-	--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-	--  - ci'  - [C]hange [I]nside [']quote
-	require('mini.ai').setup { n_lines = 500 }
-	-- Add/delete/replace surroundings (brackets, quotes, etc.)
-	--
-	-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-	-- - sd'   - [S]urround [D]elete [']quotes
-	-- - sr)'  - [S]urround [R]eplace [)] [']
-	require('mini.surround').setup()
-    end
+	"echasnovski/mini.nvim",
+	config = function()
+		require("mini.statusline").setup()
+		-- Better Around/Inside textobjects
+		--
+		-- Examples:
+		--  - va)  - [V]isually select [A]round [)]paren
+		--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+		--  - ci'  - [C]hange [I]nside [']quote
+		require("mini.ai").setup({ n_lines = 500 })
+		-- Add/delete/replace surroundings (brackets, quotes, etc.)
+		--
+		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+		-- - sd'   - [S]urround [D]elete [']quotes
+		-- - sr)'  - [S]urround [R]eplace [)] [']
+		require("mini.surround").setup()
+		require("mini.diff").setup({
+			view = {
+				style = "sign",
+				signs = {
+					add = "▎",
+					change = "▎",
+					delete = "",
+				},
+			},
+		})
+	end,
 }
