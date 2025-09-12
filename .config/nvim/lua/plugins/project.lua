edfbe9bb6be9dc9c60121d1e788dbfde5843aa3b
Arpad Illyes <arpi@fastmail.com>
2025-09-12 15:04:38 +0200

    Comment out project.nvim plugin

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -1,8 +1,8 @@
 -- Detect project root
-return {
-  "ahmedkhalf/project.nvim",
-  init = function()
-    require("project_nvim").setup {}
-  end,
- }
-
+return {}
+-- return {
+--   "ahmedkhalf/project.nvim",
+--   init = function()
+--     require("project_nvim").setup {}
+--   end,
+--  }
